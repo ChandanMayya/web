dfa0b8f7366d25432da20ca9b4540bf81b30b79c
sandhyak <sandyaborker2000@gmail.com>
2022-07-20 16:13:04 +0530

    fibo 20 jul

---
+++ p3/a.js
@@ -0,0 +1,23 @@
+n=prompt("Enter a number minimum 2!");
+/*if(n==1)
+console.log(0);
+else if(n==2)
+{
+    console.log(0)
+    console.log(1);
+}/*
+for(i=0,j=0;i!=n;i++){
+    console.log(i+j)
+    j=i;
+}
+*/
+f1=0;
+f2=1;
+document.write("Fibonacci Series upto " + n + "is: <br/>");
+document.write(f1 + "<br/>" + f2 + "<br/>" );
+for(i = 3; i <= n; i++){
+    f3=f1+f2;
+    f1=f2;
+    f2=f3;
+    document.write(f3 + "<br/>");
+}
