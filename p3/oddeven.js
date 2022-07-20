83f573a7a93f765688b926c773ef52eb575813b6
sandhyak <sandyaborker2000@gmail.com>
2022-07-20 14:51:36 +0530

    INIT 20 jul

---
+++ p3/oddeven.js
@@ -0,0 +1,5 @@
+n=prompt("Enter a number");
+if(n%2== 0)
+    alert("Even");
+else    
+    alert("Odd");
---
+++ p3/print10.js
@@ -0,0 +1,6 @@
+n=5;
+for(i=0;i<n;i++)
+    alert(i);
+
+for(i=n;i>0;i--)
+    console.log(i);
