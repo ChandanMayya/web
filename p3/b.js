e923ca050225a6f7c9f25a9d8f17412b746f11ad
sandhyak <sandyaborker2000@gmail.com>
2022-08-03 15:13:27 +0530

    3 aug

--- p3/b.js
+++ p3/b.js
@@ -4,7 +4,7 @@ document.write("<br><table border=1><tr><th>Number</th><th>Square</th></tr>");
 m="Number | Square\n";
 for(i = 1; i <= n; i++){
     //alert("Square of "+i+"is"+i**2+"\n");
-    m+="      " + i + "       |      " + i**2 + "\n";
+    m+="      " + i + "       \t      " + i**2 + "\n";
     document.write("<tr><td>" + i + "</td><td>" + i**2 + "</td></tr>");
 }
 alert(m);
